3af1e9b2fa4a633294e2b0dd3cbc5cea4757e804
Daniel Micheal Raj <132066330+Danielmichealraj@users.noreply.github.com>
2024-03-27 14:07:20 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,5 @@ if ingrediants_list:
         st.success('Order Placed Thank you')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response.json())
-
+fv_df= st.dataframe(data= fruityvice_response.json(), use_container_width = True)
 
