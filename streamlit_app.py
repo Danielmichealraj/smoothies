b22fb1eb14dd8de54b69e7bed393129a79b6649e
Daniel Micheal Raj <132066330+Danielmichealraj@users.noreply.github.com>
2024-03-27 14:05:45 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,6 @@ if ingrediants_list:
         
         st.success('Order Placed Thank you')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
 
