13ce9fe97ff6ae043ed533cbb907fbe0a3c6fabb
Daniel Micheal Raj <132066330+Danielmichealraj@users.noreply.github.com>
2024-03-27 14:11:12 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,9 @@ if ingrediants_list:
     ingrediants_string =' '
     for fruit_name in ingrediants_list:
         ingrediants_string += fruit_name + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+fv_df= st.dataframe(data= fruityvice_response.json(), use_container_width = True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingrediants_string + """ ',' """ + name_on_order + """ ')"""
@@ -39,7 +42,5 @@ if ingrediants_list:
         session.sql(my_insert_stmt).collect()
         
         st.success('Order Placed Thank you')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df= st.dataframe(data= fruityvice_response.json(), use_container_width = True)
+
 
